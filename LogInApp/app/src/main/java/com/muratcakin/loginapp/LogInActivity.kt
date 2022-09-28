1d5f6272fcb6675d2851199748f891d0efdf100a
kodluyoruz <info@kodluyoruz.org>
2022-09-28 18:16:26 +0300

    password visibility

--- LogInApp/app/src/main/java/com/muratcakin/loginapp/LogInActivity.kt
+++ LogInApp/app/src/main/java/com/muratcakin/loginapp/LogInActivity.kt
@@ -3,30 +3,43 @@ package com.muratcakin.loginapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
 
 
-    private lateinit var ivChangePasswordVisibility: ImageView
+    // flag for visibility of password
     private var isVisibilityOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
-        ivChangePasswordVisibility  = findViewById(R.id.ivShowPassword)
-
-        ivChangePasswordVisibility.setOnClickListener {
-            if (isVisibilityOn) {
-                ivChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility)
-                isVisibilityOn = false
-            } else {
-                ivChangePasswordVisibility.setImageResource(R.drawable.ic_baseline_visibility_off)
-                isVisibilityOn = true
-            }
+        ivShowHidePassword.setOnClickListener {
+            isVisibilityOn = !isVisibilityOn
+            showPassword(isVisibilityOn)
         }
+
+        showPassword(isVisibilityOn)
+
     }
+
+
+    private fun showPassword(isShow: Boolean) {
+        if(isShow) {
+            etPassword.transformationMethod =HideReturnsTransformationMethod.getInstance()
+            ivShowHidePassword.setImageResource(R.drawable.ic_baseline_visibility_off)
+        }else {
+            etPassword.transformationMethod =PasswordTransformationMethod.getInstance()
+            ivShowHidePassword.setImageResource(R.drawable.ic_baseline_visibility)
+        }
+        etPassword.setSelection(etPassword.text.toString().length)
+    }
+
 
 
     fun signUp(view: View) {
